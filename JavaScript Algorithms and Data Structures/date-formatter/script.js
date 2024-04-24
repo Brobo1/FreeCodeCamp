2069acbc2295cde92f3eb1d1eec3b871b821bc03
brobo1 <armanahz95@gmail.com>
2024-04-24 17:51:54 +0200

    Added date formatting options and event listener in script.js

--- JavaScript Algorithms and Data Structures/date-formatter/script.js
+++ JavaScript Algorithms and Data Structures/date-formatter/script.js
@@ -6,9 +6,37 @@ const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
 
+const formattedDate = `${day}-${month}-${year}`;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+    default:
+      currentDateParagraph.textContent = formattedDate;
+  }
+});
+
+/*
 console.log(`
-	Day: ${day}\n
-	Month: ${month}\n
-	Year: ${year}\n
+	Day: ${day} \n
+	Month: ${month} \n
+	Year: ${year} \n
+	Hour: ${hours} \n
+	Minutes: ${minutes} \n
 	`);
+	
+	
+	const exampleSentence = "selur pmaCedoCeerf".split("").reverse().join("");
+console.log(exampleSentence);
+*/
