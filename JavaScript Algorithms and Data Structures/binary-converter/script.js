8283050b95df53e043dda74deb6d2a5e0c923f87
brobo1 <armanahz95@gmail.com>
2024-04-28 23:02:41 +0200

    Updated input validation in binary converter and added Enter key event listener

--- JavaScript Algorithms and Data Structures/binary-converter/script.js
+++ JavaScript Algorithms and Data Structures/binary-converter/script.js
@@ -11,8 +11,11 @@ const decimalToBinary = (input) => {
     const quotient = Math.floor(input / 2);
     const remainder = input % 2;
     inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
     input = quotient;
   }
+  console.log(`Inputs: ${inputs.join(",")}`);
 };
 
 const checkUserInput = () => {
