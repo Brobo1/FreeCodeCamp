59dd72ed19184b0197c55a74bcc9e7b7ca55cc7f
brobo1 <armanahz95@gmail.com>
2024-04-28 21:40:23 +0200

    Updated input validation in binary converter and added Enter key event listener

--- JavaScript Algorithms and Data Structures/binary-converter/script.js
+++ JavaScript Algorithms and Data Structures/binary-converter/script.js
@@ -2,12 +2,31 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-function checkUserInput() {
-  if (!numberInput.value || parseInt(numberInput.value)) {
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+    inputs.push(input);
+    input = quotient;
   }
-}
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
 
 convertBtn.addEventListener("click", checkUserInput);
+
 numberInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     checkUserInput();
