154f43dfb8b51198f65f2b32dc0da665bc843273
brobo1 <armanahz95@gmail.com>
2024-05-02 06:15:53 +0200

    Refactored sorting logic to be more concise in number-sorter script.js

--- JavaScript Algorithms and Data Structures/number-sorter/script.js
+++ JavaScript Algorithms and Data Structures/number-sorter/script.js
@@ -8,13 +8,7 @@ const sortInputArray = (event) => {
   ].map((dropdown) => Number(dropdown.value));
 
   const sortedValues = inputValues.sort((a, b) => {
-    if (a > b) {
-      return -1;
-    } else if (a < b) {
-      return 1;
-    } else {
-      return 0;
-    }
+    return a - b;
   });
 
   updateUI(sortedValues);
