9b892656ab9a9b47ecd3fe8719a3b2e48098d64a
brobo1 <armanahz95@gmail.com>
2024-05-05 20:54:27 +0200

    Add formula evaluation to spreadsheet app
    This commit introduces a new function `evalFormula` which will aid in evaluating formulas inside the cells of the spreadsheet. It also contains a regex for range identification. This functionality is a critical enhancement for the spreadsheet application.

--- JavaScript Algorithms and Data Structures/spreadsheet-app/script.js
+++ JavaScript Algorithms and Data Structures/spreadsheet-app/script.js
@@ -26,6 +26,11 @@ const charRange = (start, end) =>
     String.fromCharCode(code),
   );
 
+const evalFormula = (x, cells) => {
+  const idToText = (id) => cells.find((cell) => cell.id === id).value;
+  const rangeRegex = /([A-J])/;
+};
+
 window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = (name) => {
