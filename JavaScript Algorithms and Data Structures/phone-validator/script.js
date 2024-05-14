70bdbed29c3024d20f98b6956845dc42da9e681b
brobo1 <armanahz95@gmail.com>
2024-05-14 16:16:34 +0200

    Update phone number validation logic and user feedback
    Extra validation steps have been added to the phone number validation logic, including cases for empty strings and non-US numbers. Also, user feedback has been tweaked to clearly distinguish between valid and invalid phone numbers. Clear button event listener is also added to ease the user in clearing input field.

--- JavaScript Algorithms and Data Structures/phone-validator/script.js
+++ JavaScript Algorithms and Data Structures/phone-validator/script.js
@@ -13,16 +13,19 @@ const numberToCheck = [
   "(555)555-5555",
   "as12312dasd",
   "12as12312dasd",
+  "55555555",
 ];
 
 const validate = (str) => {
   if (str === "") {
     alert("Please provide a phone number");
+    return false;
   }
+  return true;
 };
 
 const numberRegex = (nums) => {
-  const regex = /\d?\s?(\d{3}|\(\d{3}\))\s?-?(\d{3})\s?-?(\d{4})/;
+  const regex = /^1?\s?(\d{3}|\(\d{3}\))\s?-?(\d{3})\s?-?(\d{4})$/;
   let copy = [];
   nums.map((num) => {
     copy.push({ nr: num, regex: regex.test(num) });
@@ -30,8 +33,28 @@ const numberRegex = (nums) => {
   return copy;
 };
 
+const numberRegexSingle = (num) => {
+  const regex = /^1?\s?(\d{3}|\(\d{3}\))\s?-?(\d{3})\s?-?(\d{4})$/;
+  return regex.test(num);
+};
+
+const showRes = (num) => {
+  if (!validate(num)) {
+    return;
+  }
+
+  if (numberRegexSingle(num)) {
+    resultDiv.innerHTML = `<p>Valid US number: ${num}</p>`;
+  } else {
+    resultDiv.innerHTML = `<p>Invalid US number: ${num}</p>`;
+  }
+};
+
 console.log(numberRegex(numberToCheck));
 
 checkBtn.addEventListener("click", () => {
-  validate(numberInput.value);
+  showRes(numberInput.value);
+});
+clearBtn.addEventListener("click", () => {
+  resultDiv.innerHTML = "";
 });
