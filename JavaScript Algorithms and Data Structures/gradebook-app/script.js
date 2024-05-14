ca02e8c6e785e3d939017a7173e2dd0542170940
brobo1 <armanahz95@gmail.com>
2024-05-14 19:30:23 +0200

    Added new grade checking functions in script.js
    Implemented two new functions, `hasPassingGrade` and `studentMsg`, in the Script.js file. The first function assesses if the student's score is passing or not. The second function combines all the messages related to student scores into one. Both of them have been tested and they work as expected.

--- JavaScript Algorithms and Data Structures/gradebook-app/script.js
+++ JavaScript Algorithms and Data Structures/gradebook-app/script.js
@@ -18,9 +18,23 @@ function getGrade(score) {
   }
 }
 
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+
+function studentMsg(totalScores, studentScore) {
+  return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(studentScore)}. ${hasPassingGrade(studentScore) ? "You passed the course." : "You failed the course."}`;
+}
+
 console.log("avg", getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log("avg", getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
 
 console.log("range", getGrade(96));
 console.log("range", getGrade(82));
 console.log("range", getGrade(56));
+
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
