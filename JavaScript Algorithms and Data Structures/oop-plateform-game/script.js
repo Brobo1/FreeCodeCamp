43c12f38a4b8e5f62ba5d0920525da12d9eb63be
brobo1 <armanahz95@gmail.com>
2024-05-21 00:18:56 +0200

    Add player class and proportional size function to game script
    A new `Player` class and `proportionalSize` function have been added to the game script. The `proportionalSize` function is used to adjust game objects' size according to the screen resolution. Additionally, a boolean variable for checkpoint collision detection has been declared and set to true.

--- JavaScript Algorithms and Data Structures/oop-plateform-game/script.js
+++ JavaScript Algorithms and Data Structures/oop-plateform-game/script.js
@@ -10,3 +10,11 @@ const ctx = canvas.getContext();
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 const gravity = 0.5;
+
+let isCheckpointCollisionDetectionActive = true;
+
+const proportionalSize = (size) => {
+  return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+};
+
+class Player {}
