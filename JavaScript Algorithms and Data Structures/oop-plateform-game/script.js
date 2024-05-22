12389198782213a4feefd660f12f9133310599bb
brobo1 <armanahz95@gmail.com>
2024-05-22 14:57:33 +0200

    Add platform class and integrate it with animation
    Added a new class for platform with properties like position, width, height and method to draw the platform. Integrated this new class into the animation loop to draw platforms on screen. Also made a minor adjustment to improve player's x-velocity update.

--- JavaScript Algorithms and Data Structures/oop-plateform-game/script.js
+++ JavaScript Algorithms and Data Structures/oop-plateform-game/script.js
@@ -56,11 +56,48 @@ class Player {
   }
 }
 
+class Platform {
+  constructor(x, y) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 200;
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = "#acd157";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
 const player = new Player();
+
+const platformPositions = [
+  { x: 500, y: proportionalSize(450) },
+  { x: 700, y: proportionalSize(400) },
+  { x: 850, y: proportionalSize(350) },
+  { x: 900, y: proportionalSize(350) },
+  { x: 1050, y: proportionalSize(150) },
+  { x: 2500, y: proportionalSize(450) },
+  { x: 2900, y: proportionalSize(400) },
+  { x: 3150, y: proportionalSize(350) },
+  { x: 3900, y: proportionalSize(450) },
+  { x: 4200, y: proportionalSize(400) },
+  { x: 4400, y: proportionalSize(200) },
+  { x: 4700, y: proportionalSize(150) },
+];
+
+const platforms = platformPositions.map(
+  (platform) => new Platform(platform.x, platform.y),
+);
 
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  platforms.forEach((platform) => platform.draw());
+
   player.update();
 
   if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
@@ -90,6 +127,7 @@ const movePlayer = (key, xVelocity, isPressed) => {
     player.velocity.y = 0;
     return;
   }
+
   switch (key) {
     case "ArrowLeft":
       keys.leftKey.pressed = isPressed;
@@ -106,21 +144,24 @@ const movePlayer = (key, xVelocity, isPressed) => {
     case "ArrowRight":
       keys.rightKey.pressed = isPressed;
       if (xVelocity === 0) {
-        player.velocity.x += xVelocity;
+        player.velocity.x = xVelocity;
       }
+      player.velocity.x += xVelocity;
   }
 };
 
 const startGame = () => {
   canvas.style.display = "block";
   startScreen.style.display = "none";
-  player.draw();
+  animate();
 };
 
 startBtn.addEventListener("click", startGame);
+
 window.addEventListener("keydown", ({ key }) => {
   movePlayer(key, 8, true);
 });
+
 window.addEventListener("keyup", ({ key }) => {
   movePlayer(key, 0, false);
 });
