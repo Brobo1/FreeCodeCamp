9e4e43d6b775fe9e618398b6402dac53532fa585
brobo1 <armanahz95@gmail.com>
2024-05-22 17:45:40 +0200

    Add CheckPoint class and integrate into game logic
    A new class, CheckPoint, has been added into the platform game's script.js. This class has constructor, draw, and claim methods. Instances of CheckPoint were created and integrated into the animation and collision detection logic within the game to enhance gameplay.

--- JavaScript Algorithms and Data Structures/oop-plateform-game/script.js
+++ JavaScript Algorithms and Data Structures/oop-plateform-game/script.js
@@ -71,6 +71,26 @@ class Platform {
   }
 }
 
+class CheckPoint {
+  constructor(x, y, z) {
+    this.position = { x, y };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(70);
+    this.claimed = false;
+  }
+
+  draw() {
+    ctx.fillStyle = "#f1be32";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  claim() {
+    this.width = 0;
+    this.height = 0;
+    this.position.y = Infinity;
+    this.claimed = true;
+  }
+}
+
 const player = new Player();
 
 const platformPositions = [
@@ -92,6 +112,16 @@ const platforms = platformPositions.map(
   (platform) => new Platform(platform.x, platform.y),
 );
 
+const checkpointPositions = [
+  { x: 1170, y: proportionalSize(80), z: 1 },
+  { x: 2900, y: proportionalSize(330), z: 2 },
+  { x: 4800, y: proportionalSize(80), z: 3 },
+];
+
+const checkpoints = checkpointPositions.map(
+  (checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z),
+);
+
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -99,6 +129,7 @@ const animate = () => {
   platforms.forEach((platform) => {
     platform.draw();
   });
+  checkpoints.forEach((checkpoint) => (checkpoint.position.x -= 5));
 
   player.update();
 
@@ -145,6 +176,8 @@ const animate = () => {
       player.position.y <= platform.position.y + platform.height,
     ];
     if (platformDetectionRules.every((rule) => rule)) {
+      player.position.y = platform.position.y + player.height;
+      player.velocity.y = gravity;
     }
   });
 };
