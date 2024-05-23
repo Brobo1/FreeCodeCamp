514ac82e9efcc74c296c2ea23662fa36e2c05d63
brobo1 <armanahz95@gmail.com>
2024-05-23 18:39:52 +0200

    Update game title and add game logic functions
    The title of the dice game has been updated from "Advanced Dice Game" to "Dice Game". In addition, the logic of the game has been enhanced with two functions: `updateRadioOption()` which enables and updates radio selection options, and `getHighestDuplicates()` which counts and returns the highest count of repeating numbers in an array.

--- JavaScript Algorithms and Data Structures/dice-game/script.js
+++ JavaScript Algorithms and Data Structures/dice-game/script.js
@@ -46,7 +46,29 @@ const updateStats = () => {
   currentRoundText.textContent = round;
 };
 
-const updateRadioOption = (optionNode, score) => {};
+const updateRadioOption = (optionNode, score) => {
+  scoreInputs[optionNode].disabled = false;
+  scoreInputs[optionNode].value = score;
+  scoreSpans[optionNode].textContent = `, score = ${score}`;
+};
+
+const getHighestDuplicates = (arr) => {
+  const counts = {};
+  for (const num of arr) {
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  }
+  let highestCount = 0;
+  for (const num of arr) {
+    const count = counts[num];
+    if (count >= 3 && count > highestCount) {
+      highestCount = count;
+    }
+  }
+};
 
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
