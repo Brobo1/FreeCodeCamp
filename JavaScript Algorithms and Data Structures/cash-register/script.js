6c673a35c64f87ff4e11458848a72402e4c084e0
brobo1 <armanahz95@gmail.com>
2024-05-26 16:04:30 +0200

    Add cash register functionality and styles
    Extended the cash register script to include a checker for insufficient customer cash, update displayed cash, and trigger these actions upon clicking the purchase button. Enhanced the visual representation of available change in the till with the addition of CSS styles.

--- JavaScript Algorithms and Data Structures/cash-register/script.js
+++ JavaScript Algorithms and Data Structures/cash-register/script.js
@@ -10,7 +10,43 @@ let cid = [
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
 ];
+const denom = [
+  ["PENNY", 0.01],
+  ["NICKEL", 0.05],
+  ["DIME", 0.1],
+  ["QUARTER", 0.25],
+  ["ONE", 1],
+  ["FIVE", 5],
+  ["TEN", 10],
+  ["TWENTY", 20],
+  ["ONE HUNDRED", 100],
+];
 
 const cash = document.getElementById("cash");
 const due = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
+const till = document.getElementById("till");
+
+const noCash = () => {
+  if (cash.value < price) {
+    alert("Customer does not have enough money to purchase the item");
+  }
+};
+
+const showCash = () => {
+  till.innerHTML = "";
+  for (const left of cid) {
+    till.innerHTML += `<p>${left[0]}: ${left[1]}</p>`;
+  }
+};
+
+showCash();
+
+const updateChange = () => {
+  showCash();
+};
+
+purchaseBtn.addEventListener("click", () => {
+  noCash();
+  updateChange();
+});
