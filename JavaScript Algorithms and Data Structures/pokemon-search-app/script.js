b6437ed13fc036d11d01471080f5bd6720168ac4
Brobo1 <armanahz95@gmail.com>
2024-06-03 19:17:24 +0200

    Update Pokemon search app to display more information
    This commit updates the Pokemon search application in order to display more details about the search results. The update includes presenting the Pokemon's weight, height, type, and stats such as attack, defense, and speed. The code now dynamically populates this data into a container in the HTML, enhancing functionality and user experience.

--- JavaScript Algorithms and Data Structures/pokemon-search-app/script.js
+++ JavaScript Algorithms and Data Structures/pokemon-search-app/script.js
@@ -1,31 +1,46 @@
 const input = document.getElementById("search-input");
 const btn = document.getElementById("search-button");
-const pName = document.getElementById("pokemon-name");
-const pId = document.getElementById("pokemon-id");
-
+const container = document.getElementById("container");
 const fetchData = async () => {
   try {
     const res = await fetch(
       "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon",
     );
     const data = await res.json();
+    console.log(data);
     logNames(data);
   } catch (err) {
     console.log(err);
   }
 };
 const logNames = (data) => {
-  const { count, results } = data;
+  const { results } = data;
   const name = results.find(
     (pokemon) =>
       pokemon.name === input.value.toLowerCase() ||
       pokemon.id.toString() === input.value.toLowerCase(),
   );
   if (name) {
-    console.log(name);
+    return fillData(name);
   } else {
     alert("Pokémon not found");
   }
+};
+
+const fillData = (pokemon) => {
+  container.innerHTML = `
+    <p class="pokemon-name">${pokemon.name}</p>
+    <p class="pokemon-id">${pokemon.id}</p>
+    <p class="weight">${pokemon.weight}</p>
+    <p class="height">${pokemon.height}</p>
+    <p class="types">${pokemon.type}</p>
+    <p class="hp">${pokemon}</p>
+    <p class="attack">${pokemon}</p>
+    <p class="defense">${pokemon}</p>
+    <p class="special-attack">${pokemon}</p>
+    <p class="special-defense">${pokemon}</p>
+    <p class="speed">${pokemon}</p>
+  `;
 };
 
 btn.addEventListener("click", fetchData);
